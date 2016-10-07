4ff474c504bb6c290d3928c02dc415c2544d490d
vitalik26ua <vitya.yatskiv1@yandex.ua>
2016-10-08 00:02:30 +0300

    Add files via upload

---
+++ portfolio/sites/supershop/admin-panel/js/scripts.js
@@ -0,0 +1,84 @@
+function order()  {
+	var el = document.getElementById("orders_i");
+el.style.cssText="color: #2db86b";
+	var el = document.getElementById("orders_a");
+el.style.cssText="color: #fff";
+	var el = document.getElementById("c1");
+el.style.cssText="position: static; display: block";
+	var el = document.getElementById("c3");
+el.style.cssText="display: none";
+	var el = document.getElementById("c2");
+el.style.cssText="display: none";
+	var el = document.getElementById("commodity_i");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("users_im");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("order");
+el.style.cssText="background-color: #243342";
+	var el = document.getElementById("users");
+el.style.cssText="background-color: #34495E";
+	var el = document.getElementById("commodity");
+el.style.cssText="background-color: #34495E";
+	var el = document.getElementById("users_a");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("commodity_a");
+el.style.cssText="color: #95a5a6";
+}
+
+function users()  {
+	var el = document.getElementById("users_a");
+el.style.cssText="color: #fff";
+  var el = document.getElementById("users_im");
+el.style.cssText="color: #2db86b";
+    var el = document.getElementById("orders_i");
+el.style.cssText="color: #95a5a6";
+  var el = document.getElementById("orders_a");
+el.style.cssText="color: #95a5a6";
+  var el = document.getElementById("commodity_i");
+el.style.cssText="color: #95a5a6";
+    var el = document.getElementById("c2");
+el.style.cssText="position: static; display: block";
+    var el = document.getElementById("c1");
+el.style.cssText="display: none";
+    var el = document.getElementById("c3");
+el.style.cssText="display: none";
+    var el = document.getElementById("users");
+el.style.cssText="background-color: #243342";
+  var el = document.getElementById("commodity");
+el.style.cssText="background-color: #34495E";
+  var el = document.getElementById("order");
+el.style.cssText="background-color: #34495E";
+	var el = document.getElementById("orders_a");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("commodity_a");
+el.style.cssText="color: #95a5a6";
+ }
+
+function commodity(){
+  var el = document.getElementById("commodity_i");
+el.style.cssText="color: #2db86b";
+	var el = document.getElementById("orders_a");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("c3");
+el.style.cssText="position: static; display: block";
+  var el = document.getElementById("c1");
+el.style.cssText="display: none";
+  var el = document.getElementById("c2");
+el.style.cssText="display: none";
+  var el = document.getElementById("orders_i");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("users_im");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("commodity");
+el.style.cssText="background-color: #243342";
+	var el = document.getElementById("users");
+el.style.cssText="background-color: #34495E";
+	var el = document.getElementById("order");
+el.style.cssText="background-color: #34495E";
+	var el = document.getElementById("users_a");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("orders_a");
+el.style.cssText="color: #95a5a6";
+	var el = document.getElementById("commodity_a");
+el.style.cssText="color: #fff";
+}
