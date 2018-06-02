033a39aaa2e4797de0c5650c3d4a6e7ad5a6d405
vitalik26ua <vitya.yatskiv1@yandex.ua>
2018-06-03 02:46:13 +0300

    1

--- test/js/common.js
+++ test/js/common.js
@@ -3,4 +3,17 @@ $(function() {
   $("#twent").twentytwenty();
 
 
+  $(".qr").click(function () {
+    new QRCode(document.getElementById("qrcode"), this.dataset.url);
+     $('#qrcode').show();
+    return false;
+  });
+
+  $(document).click( function(event){
+    if( $(event.target).closest("#qrcode").length ) return;
+    $("#qrcode").hide();
+    event.stopPropagation();
+    $("#qrcode").empty()
+  });
+
 });
